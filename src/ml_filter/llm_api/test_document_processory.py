13cd54d107bdfd6f52a9a57cbd80123a8064f9c3
mali-git <mehdi.ali@iais.fraunhofer.de>
2024-10-04 20:38:37 +0200

    refactor: add correct test

--- src/ml_filter/llm_api/test_document_processory.py
+++ src/ml_filter/llm_api/test_document_processory.py
@@ -1,23 +1,44 @@
-import os
 from pathlib import Path
+from unittest.mock import Mock
 
 import pytest
 
-from ml_filter.classifier_training_pipeline import ClassifierTrainingPipeline
-
-os.environ["CUDA_VISIBLE_DEVICES"] = "0"
-working_dir = Path(os.path.dirname(__file__))
-
-
-@pytest.fixture
-def classifier_training_pipeline():
-    return ClassifierTrainingPipeline(working_dir / "resources" / "configs" / "test_config.yaml")
+from ml_filter.data_processing.document_processor import DocumentProcessor
+from ml_filter.data_processing.prompt_builder import PromptBuilder
+from ml_filter.llm_api.llm_rest_client import LLMRestClient
+from ml_filter.tokenizer.tokenizer_wrapper import PreTrainedHFTokenizer
 
 
-def test_train_classifier(classifier_training_pipeline):
+def test_run():
+    llm_rest_client = Mock(spec=LLMRestClient)
+    expected_score = 5
+    HERE = Path(__file__).parent
+    out_path = HERE / "cache/predictions.txt"
+    llm_rest_client.generate = lambda prompt: {
+        "generated_text": f"{p['content']} score:{expected_score}" for p in prompt
+    }
+    llm_rest_client.tokenizer = Mock(spec=PreTrainedHFTokenizer)
+    llm_rest_client.tokenizer.truncation = False
+    llm_rest_client.tokenizer.padding = False
+    llm_rest_client.tokenizer.max_length = 100
+
+    prompt_builder = Mock(spec=PromptBuilder)
+    prompt_builder.construct_prompt = lambda text: [{"role": "user", "content": text}]
+    document_processor = DocumentProcessor(
+        llm_rest_client=llm_rest_client,
+        prompt_builder=prompt_builder,
+        queue_size=2,
+        batch_size=2,
+        output_file_path=Path(out_path),
+        num_processes=1,
+    )
+
+    data = [
+        {"text": "Hello world!"},
+        {"text": "This is a test."},
+    ]
+
     try:
-        # Act: Call the train method
-        classifier_training_pipeline.train_classifier()
-    except Exception as e:
-        # Fail the test if any exception occurs
-        pytest.fail(f"Training raised an unexpected exception: {e}")
+        document_processor.run(data)
+    except Exception:
+        pytest.fail("document_processor.run() raised an exception unexpectedly")
