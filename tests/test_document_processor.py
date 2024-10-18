4a1775fc09e0f2bccac8e02b1b7c51beaf043d6b
mali-git <mehdi.ali@iais.fraunhofer.de>
2024-10-18 09:36:42 +0200

    refactor: remove duplicate file

--- tests/test_document_processor.py
+++ tests/test_document_processor.py
@@ -4,6 +4,7 @@ from unittest.mock import Mock
 import pytest
 
 from ml_filter.data_processing.document_processor import DocumentProcessor
+from ml_filter.data_processing.llm_score_metrics import EducationalScoreMetric
 from ml_filter.data_processing.prompt_builder import PromptBuilder
 from ml_filter.llm_api.llm_rest_client import LLMRestClient
 from ml_filter.tokenizer.tokenizer_wrapper import PreTrainedHFTokenizer
@@ -43,3 +44,34 @@ def test_run():
         document_processor.run(data)
     except Exception:
         pytest.fail("document_processor.run() raised an exception unexpectedly")
+
+
+def test_find_last_pattern():
+    text = "Hello world! This is a test."
+    score_metric = EducationalScoreMetric()
+    document_processor = DocumentProcessor(
+        llm_rest_client=Mock(spec=LLMRestClient),
+        prompt_builder=Mock(spec=PromptBuilder),
+        queue_size=2,
+        batch_size=2,
+        output_file_path=Mock(spec=Path("cache/predictions.txt")),
+        num_processes=1,
+        score_metric_name="educational_score",
+    )
+
+    assert document_processor.find_last_pattern(text, score_metric.pattern) is None
+
+    text = "Hello world! This is a test. score:5"
+    assert document_processor.find_last_pattern(text, score_metric.pattern) is None
+
+    text = "Hello world! This is a test. Educational score:5"
+    assert document_processor.find_last_pattern(text, score_metric.pattern) == "5"
+
+    text = "Hello world! This is a test. Educational score:2/5"
+    assert document_processor.find_last_pattern(text, score_metric.pattern) == "2"
+
+    text = "Hello world! Educational score:2/5. This is a test. Educational score:4/5"
+    assert document_processor.find_last_pattern(text, score_metric.pattern) == "4"
+
+    text = "Hello world! This is a test. Educational ScoRe: 5/1"
+    assert document_processor.find_last_pattern(text, score_metric.pattern) is None
