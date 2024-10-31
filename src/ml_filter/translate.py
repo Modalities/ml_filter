1cc9af12e5885ef2a1a12760000263b16fb065cc
mali-git <mehdi.ali@iais.fraunhofer.de>
2024-10-31 18:21:50 +0100

    refactor: DeepL translator and adapt OpenaAI translator

--- src/ml_filter/translate.py
+++ src/ml_filter/translate.py
@@ -6,6 +6,8 @@ import deepl
 import openai
 import yaml
 
+from constants import EUROPEAN_LANGUAGES
+
 
 class Translator(ABC):
     """An abstract class for translating text into multiple languages."""
@@ -14,9 +16,9 @@ class Translator(ABC):
         self,
         api_key: str,
         input_path: Path,
-        source_language: str,
-        languages: list[str],
-        tag_to_ignore: str | None,
+        source_language_code: str,
+        language_codes: list[str],
+        ignore_tag: str | None,
     ):
         """Initialize the translation class with the given parameters.
 
@@ -29,9 +31,9 @@ class Translator(ABC):
         """
         self.api_key = api_key
         self.input_path = input_path
-        self.source_language = source_language
-        self.languages = languages
-        self.tag_to_ignore = tag_to_ignore
+        self.source_language_code = source_language_code
+        self.language_codes = language_codes
+        self.ignore_tag = ignore_tag
 
     @abstractmethod
     def translate(self) -> dict[str, str]:
@@ -72,6 +74,66 @@ class Translator(ABC):
         return data
 
 
+class DeepLTranslator(Translator):
+    """A class to translate text into multiple languages using the DeepL API."""
+
+    def __init__(
+        self,
+        api_key: str,
+        input_path: Path,
+        source_language_code: str,
+        language_codes: list[str],
+        tag_to_ignore: str | None,
+    ):
+        super().__init__(api_key, input_path, source_language_code, language_codes, tag_to_ignore)
+        self._client = deepl.Translator(self.api_key)
+
+    @property
+    def client(self):
+        return self._client
+
+    def translate(self) -> dict[str, str]:
+        """Translate the given text into multiple languages using the DeepL API."""
+        translated_data = {}
+        data = self._load_data()
+        text = data["prompt"]
+
+        ignore_tag = self._get_ignore_tag(ignore_tag=self.ignore_tag)
+        tag_handling = self._get_tag_handling_strategy(ignore_tag=self.ignore_tag)
+
+        if ignore_tag is not None:
+            ignore_tag = [ignore_tag]
+
+        for lang_code in self.language_codes:
+            result = self.client.translate_text(
+                text,
+                source_lang=self.source_language_code,
+                target_lang=lang_code,
+                tag_handling=tag_handling,
+                ignore_tags=ignore_tag,
+            )
+            translated_data[lang_code] = result.text
+
+        return translated_data
+
+    def _get_tag_handling_strategy(self, ignore_tag: str | None):
+        return "xml" if ignore_tag is not None else None
+
+    def _get_ignore_tag(self, ignore_tag: str | None) -> str | None:
+        if ignore_tag is None:
+            return None
+
+        match = re.search(r"<(.*)>", ignore_tag)
+
+        if match:
+            result = match.group(1)
+            ignore_tag = result
+        else:
+            raise ValueError("No tag found in the text.")
+
+        return ignore_tag
+
+
 class OpenAITranslator(Translator):
     """A class to translate text into multiple languages using the OpenAI API."""
 
@@ -85,24 +147,23 @@ class OpenAITranslator(Translator):
     ):
         super().__init__(api_key, input_path, source_language, languages, tag_to_ignore)
 
-        self._client = openai.OpenAI()
-        openai.api_key = self.api_key
+        self._client = openai.OpenAI(api_key=api_key)
 
     @property
     def client(self):
         return self._client
 
-    def translate(self, text: str):
+    def translate(self):
         """Translate the given text into multiple languages using the OpenAI API."""
         translated_data = {}
         data = self._load_data()
         text = data["prompt"]
 
-        ignore_text = self._get_ignore_text(tag_to_ignore=self.tag_to_ignore)
+        ignore_text = self._get_ignore_text(tag_to_ignore=self.ignore_tag)
 
-        for lang in self.languages:
-            prompt = f"""Translate the following text into {lang}{ignore_text}:
-            {text}."""
+        for lang_code in self.language_codes:
+            language = EUROPEAN_LANGUAGES[lang_code]
+            prompt = f"""Translate the following text into {language}{ignore_text}: {text}."""
             # Call the API
             response = self.client.chat.completions.create(
                 model="gpt-4o",
@@ -111,62 +172,15 @@ class OpenAITranslator(Translator):
                     {"role": "user", "content": prompt},
                 ],
             )
-            translated_data[lang] = response.choices[0].message
+            translated_data[lang_code] = response.choices[0].message
+
+        return translated_data
 
     def _get_ignore_text(self, tag_to_ignore: str | None) -> str:
         if tag_to_ignore is not None:
             closing_tag_to_ignore = re.sub(r"<(\w+)>", r"</\1>", tag_to_ignore)
             # Add whitespace at the beginning since the text is appended to the prompt
-            ignore_text = (
-                f'. Text that is within "{self.tag_to_ignore} {closing_tag_to_ignore}" should not be translated:'
-            )
+            ignore_text = f". Text that is within '{self.ignore_tag} {closing_tag_to_ignore}' should not be translated"
         else:
-            ignore_text = ":"
+            ignore_text = ""
         return ignore_text
-
-
-class DeepLTranslator(Translator):
-    """A class to translate text into multiple languages using the DeepL API."""
-
-    def __init__(
-        self, api_key: str, input_path: Path, source_language: str, languages: list[str], tag_to_ignore: str | None
-    ):
-        super().__init__(api_key, input_path, source_language, languages, tag_to_ignore)
-        self._client = deepl.Translator(self.api_key)
-
-    @property
-    def client(self):
-        return self._client
-
-    def translate(self) -> dict[str, str]:
-        """Translate the given text into multiple languages using the DeepL API."""
-        translated_data = {}
-        data = self._load_data()
-        text = data["prompt"]
-
-        ignore_tags, tag_handling = (
-            self._get_ignore_tags(tag_to_ignore=self.tag_to_ignore) if self.tag_to_ignore is not None else (None, None)
-        )
-
-        for lang in self.languages:
-            result = self.client.translate_text(
-                text,
-                source_lang=self.source_language,
-                target_lang=lang,
-                tag_handling=tag_handling,
-                ignore_tags=ignore_tags,
-            )
-            translated_data[lang] = result.text
-
-        return translated_data
-
-    def _get_ignore_tags(self, tag_to_ignore: str) -> tuple[list[str], str]:
-        tag_handling = "xml"
-        match = re.search(r"<(.*)>", tag_to_ignore)
-        if match:
-            result = match.group(1)
-            ignore_tags = [result]
-        else:
-            raise ValueError("No tag found in the text.")
-
-        return ignore_tags, tag_handling
