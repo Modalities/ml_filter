ab48fb1836750161f93396d15ee2a09e8e65c95f
davidkaczer <dkaczer@gmail.com>
2024-11-12 16:24:58 +0100

    feat: add debug classifier dataset and creation script
    - new multiscore format
    - closes #47

---
+++ data/create_dummy_dataset.py
@@ -0,0 +1,28 @@
+import json
+import os
+import random
+from pathlib import Path
+
+working_dir = Path(os.path.dirname(__file__))
+
+source_dataset_path = working_dir / "lorem_ipsum.jsonl"
+destination_path = working_dir / "lorem_ipsum_multiscore.jsonl"
+
+scoring_keys = [
+    "edu_en",
+    "edu_de",
+    "toxicity_en",
+    "toxicity_de",
+]
+
+with open(source_dataset_path, "r") as f:
+    data = [json.loads(line) for line in f]
+
+for record in data:
+    record.pop("score", None)
+    scores = [random.randint(0, 5) for _ in scoring_keys]
+    record["scores"] = {k: v for k, v in zip(scoring_keys, scores)}
+
+with open(destination_path, "w") as f:
+    for record in data:
+        f.write(json.dumps(record) + "\n")
