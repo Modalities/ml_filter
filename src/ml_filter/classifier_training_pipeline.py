fix: logit masking in custom classifier layer

src/ml_filter/classifier_training_pipeline.py
@@ -8,7 +8,8 @@ from datasets import Dataset, load_dataset
 from omegaconf import OmegaConf
 from transformers import AutoModelForSequenceClassification, DataCollatorWithPadding, Trainer, TrainingArguments
 
-from .tokenizer.tokenizer_wrapper import PreTrainedHFTokenizer
+from ml_filter.tokenizer.tokenizer_wrapper import PreTrainedHFTokenizer
+from ml_filter.utils.train_classifier import LogitMaskLayer
 
 sys.path.append(os.path.join(os.getcwd(), "src"))
 
@@ -22,7 +23,6 @@ class ClassifierTrainingPipeline:
         self.val_data_file_path = cfg.data.val_file_path
         self.label_column = cfg.data.label_column
 
-        device = "cuda" if torch.cuda.is_available() else "cpu"
         # Model
         # TODO: Check, whetehr AutoModelForSequenceClassification is general enough
         self.model = AutoModelForSequenceClassification.from_pretrained(
@@ -38,17 +38,14 @@ class ClassifierTrainingPipeline:
 
         if self.num_metrics > 1:
             self.num_classes_per_metric = torch.tensor(cfg.data.num_classes_per_metric)
-            self.max_num_labels_per_metric = max(self.num_classes_per_metric)
-            self.model.num_labels = self.num_metrics * self.max_num_labels_per_metric
+        elif self.num_metrics == 1:
+            self.num_classes_per_metric = torch.tensor(cfg.model.num_labels).unsqueeze(0)
+        self.model.num_labels = self.num_metrics * max(self.num_classes_per_metric)
 
-            self.model.classifier = torch.nn.Linear(768, self.model.num_labels, bias=True)
-            self.raw_logit_mask = (
-                torch.arange(self.max_num_labels_per_metric).repeat(self.num_metrics, 1).T < self.num_classes_per_metric
-            ).to(device)
-            self.logit_mask = (self.raw_logit_mask + 1e-45).log()
-        else:
-            self.max_num_labels_per_metric = cfg.model.num_labels
-            self.logit_mask = torch.zeros(self.max_num_labels_per_metric, self.num_metrics).to(device)
+        self.model.classifier = torch.nn.Sequential(
+            torch.nn.Linear(768, self.model.num_labels, bias=True),
+            LogitMaskLayer(self.num_classes_per_metric),
+        )
 
         # Tokenizer
         self.tokenizer = PreTrainedHFTokenizer(
@@ -117,9 +114,6 @@ class ClassifierTrainingPipeline:
 
         return dataset.map(process_batch, batched=True)
 
-    def reshape_and_mask_logits(self, logits):
-        return logits.view(-1, self.max_num_labels_per_metric, self.num_metrics) + self.logit_mask
-
     def multi_target_cross_entropy_loss(
         self,
         input,
@@ -128,7 +122,7 @@ class ClassifierTrainingPipeline:
         **kwargs,
     ):
         return torch.nn.functional.cross_entropy(
-            self.reshape_and_mask_logits(input["logits"]),
+            input["logits"],
             target.view(-1, self.num_metrics),
         )
 

tests/test_classifier_training_pipeline.py
@@ -21,17 +21,15 @@ def classifier_training_pipeline_multiscore():
 
 
 def test_multiscore_logit_masking(classifier_training_pipeline_multiscore):
-    device = "cuda" if torch.cuda.is_available() else "cpu"
-    dummy_input_ids = torch.tensor([[1, 2, 3], [5, 6, 7]]).to(device)
+    dummy_input_ids = torch.tensor([[1, 2, 3], [5, 6, 7]])
     batch_size = dummy_input_ids.shape[0]
 
-    classifier_training_pipeline_multiscore.model.to(device)
     output = classifier_training_pipeline_multiscore.model(dummy_input_ids)
-    logits = classifier_training_pipeline_multiscore.reshape_and_mask_logits(output["logits"])
+    logits = output["logits"]
 
     assert logits.shape == (
         batch_size,
-        classifier_training_pipeline_multiscore.max_num_labels_per_metric,
+        max(classifier_training_pipeline_multiscore.num_classes_per_metric),
         classifier_training_pipeline_multiscore.num_metrics,
     )
     eps = 1e-30
@@ -40,10 +38,18 @@ def test_multiscore_logit_masking(classifier_training_pipeline_multiscore):
         assert (torch.softmax(logits, -1)[:, :n_classes, i] > eps).all()
 
 
-def test_train_classifier(classifier_training_pipeline, classifier_training_pipeline_multiscore):
+def test_train_classifier(classifier_training_pipeline):
     try:
         # Act: Call the train method
         classifier_training_pipeline.train_classifier()
+    except Exception as e:
+        # Fail the test if any exception occurs
+        pytest.fail(f"Training raised an unexpected exception: {e}")
+
+
+def test_train_classifier_multiscore(classifier_training_pipeline_multiscore):
+    try:
+        # Act: Call the train method
         classifier_training_pipeline_multiscore.train_classifier()
     except Exception as e:
         # Fail the test if any exception occurs