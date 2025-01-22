1e0490d5460fda9b56927a0823a78daa6a4c548b
Florian Mai <fmai@bit.uni-bonn.de>
2025-01-22 14:59:47 +0100

    Moved dummy data to centralized folder, fixed #172

--- src/ml_filter/inference_pipeline/create_dummy_ds.py
+++ src/ml_filter/inference_pipeline/create_dummy_ds.py
@@ -14,5 +14,8 @@ def create_dummy_dataset(num_examples: int, seq_length: int, save_path: str | os
 
 
 # Example usage:
-os.makedirs('datasets', exist_ok=True)
-create_dummy_dataset(num_examples=10000, seq_length=512, save_path='datasets/dummy_dataset1')
+# Create the directory in the root folder - going up 3 levels from current directory
+root_dir = os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
+save_dir = os.path.join(root_dir, 'data', 'inference_pipeline')
+os.makedirs(save_dir, exist_ok=True)
+create_dummy_dataset(num_examples=10000, seq_length=512, save_path=os.path.join(save_dir, 'dummy_dataset1'))
