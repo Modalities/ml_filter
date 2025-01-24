5729ba03548a0139177e5965a2eb7e4f8ef35fac
davidkaczer <dkaczer@gmail.com>
2025-01-24 16:10:00 +0100

    fix: adapt slurm script for mn5

--- src/ml_filter/inference_pipeline/runner.sh
+++ src/ml_filter/inference_pipeline/runner.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
-#SBATCH --job-name=bert_inference
-#SBATCH --nodes=16
-#SBATCH --ntasks-per-node=4
-#SBATCH --cpus-per-task=4
-#SBATCH --gpus-per-task=1
-#SBATCH --time=24:00:00
+#SBATCH --job-name=bert_inference_test
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=20
+#SBATCH --gres gpu:1
+#SBATCH --time=00:20:00
 #SBATCH --output=logs/inference_%j.out
+#SBATCH --error=logs/inference_%j.err
 
-source ~/miniconda3/etc/profile.d/conda.sh
-conda activate torch
+source ../../../../venvs/ml_filter_build_ba236af2408b6e90856c4ef8d654e7ab7a4fbc49/bin/activate
 
 mkdir -p logs checkpoints outputs
 
@@ -19,4 +18,9 @@ srun python inference.py \
   --output_dir outputs \
   --checkpoint_dir checkpoints \
   --task_id $SLURM_PROCID \
-  --num_tasks $SLURM_NTASKS
+  --num_tasks $SLURM_NTASKS \
+  --model_checkpoint ../../../tests/cache/checkpoint-1 \
+  --model_arch "snowflake-arctic-embed" \
+  --num_regressor_outputs 4 \
+  --num_classes_per_output 6 6 2 2 \
+  --use_regression True
