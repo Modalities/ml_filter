2dda6358f25a3397ebd01953502647ce87bb70bd
mali-git <ali-mehdi@live.de>
2025-02-27 14:08:44 +0100

    fix: forward pass

--- src/ml_filter/models/annotator_models.py
+++ src/ml_filter/models/annotator_models.py
@@ -2,7 +2,7 @@ from abc import ABC, abstractmethod
 
 import torch
 import torch.nn as nn
-from transformers.modeling_utils import PreTrainedModel
+from transformers.modeling_utils import ModelOutput, PreTrainedModel
 
 
 class AnnotatorHead(nn.Module, ABC):
@@ -39,18 +39,17 @@ class AnnotatorModel(nn.Module):
         super().__init__()
         self.base_model = base_model
         self.freeze_base_model_parameters = freeze_base_model_parameters
-        self.head = head
-        self._overwrite_head()
+        self._overwrite_head(head=head)
         if self.freeze_base_model_parameters:
             self._freeze_base_model()
 
-    def _overwrite_head(self):
+    def _overwrite_head(self, head: nn.Module):
         """Replaces the classifier in the base model with the custom head."""
         if hasattr(self.base_model, "classifier"):
             if hasattr(self.base_model.classifier, "out_proj"):
-                self.base_model.classifier.out_proj = self.head
+                self.base_model.classifier.out_proj = head
             else:
-                self.base_model.classifier = self.head
+                self.base_model.classifier = head
         else:
             raise AttributeError("The base model does not have a 'classifier' attribute.")
 
@@ -75,12 +74,32 @@ class AnnotatorModel(nn.Module):
             for param in self.base_model.bert.pooler.parameters():
                 param.requires_grad = True
 
-    def forward(self, **kwargs):
-        """Passes input through the base model."""
+    def forward(
+        self,
+        input_ids: torch.Tensor | None = None,
+        attention_mask: torch.Tensor | None = None,
+        token_type_ids: torch.Tensor | None = None,
+        labels: torch.Tensor | None = None,
+        return_dict: bool | None = None,
+    ) -> ModelOutput:
+        """Forward pass through the base model.
 
-        # We are using **kwargs here because
-        # it ensures independence to signature changes in the base model's forward method.
-        return self.base_model(**kwargs)
+        Args:
+            input_ids (Tensor | None): Tokenized input IDs of shape (batch_size, sequence_length).
+            attention_mask (Tensor | None): Attention mask tensor of shape (batch_size, sequence_length).
+            token_type_ids (Tensor | None): Segment embeddings tensor (if applicable to model type).
+            labels (Tensor | None): Labels for training (if applicable, e.g., for classification).
+
+        Returns:
+            ModelOutput: Output of the pre-trained transformer model.
+        """
+        return self.base_model(
+            input_ids=input_ids,
+            attention_mask=attention_mask,
+            token_type_ids=token_type_ids,
+            labels=labels,
+            return_dict=return_dict,
+        )
 
 
 class MultiTargetRegressionHead(AnnotatorHead):
