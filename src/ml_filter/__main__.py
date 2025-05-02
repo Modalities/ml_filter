6823308104e9ec82a1cd5a4ee93b4ef8c8a92b90
mali-git <ali-mehdi@live.de>
2025-05-02 11:43:48 +0200

    refactor: remove ununsed files

--- src/ml_filter/__main__.py
+++ src/ml_filter/__main__.py
@@ -734,16 +734,5 @@ def _get_target_language_codes_list_helper(target_language_codes: str) -> list[s
     return [lang_code.strip().lower() for lang_code in target_language_codes.split(",")]
 
 
-# @main.command(name="inference_pipeline")
-# @click.option(
-#     "--config_file_path",
-#     type=click_pathlib.Path(exists=True),
-#     required=True,
-#     help="Path to a file with the YAML config file.",
-# )
-# def entry_inference_pipeline(config_file_path: Path):
-#     run_pipeline(config_file_path)
-
-
 if __name__ == "__main__":
     main()
